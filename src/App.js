c65ef539f21ee18182b9119a4323296806647cd6
unknown <saurabhjangid2000@gmail.com>
2021-01-09 16:46:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Navigation from './components/navigation';
+import Header from './components/header';
+import Features from './components/features';
+import About from './components/about';
+import Services from './components/services';
+import Gallery from './components/gallery';
+import Testimonials from './components/testimonials';
+import Team from './components/Team';
+import Contact from './components/contact';
+import JsonData from './data/data.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    website: {},
+  }
+  getwebsite() {
+    this.setState({website : JsonData})
+  }
+
+  componentDidMount() {
+    this.getwebsite();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Header data={this.state.website.Header} />
+        <Features data={this.state.website.Features} />
+        <About data={this.state.website.About} />
+        <Services data={this.state.website.Services} />
+        <Gallery />
+        <Testimonials data={this.state.website.Testimonials} />
+        <Team data={this.state.website.Team} />
+        <Contact data={this.state.website.Contact} />
+      </div>
+    )
+  }
 }
 
 export default App;
